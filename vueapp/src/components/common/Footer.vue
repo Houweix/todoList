a220b604c1b2444911a7cbd30a318e02a925dde5
houweix <houwei19960927@126.com>
2018-02-04 23:37:21 +0800

    完成主体框架

---
+++ vueapp/src/components/common/Footer.vue
@@ -0,0 +1,14 @@
+<!--公共组件之尾部样式-->
+<template>
+  <h1>footer</h1>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vueapp/src/components/common/Header.vue
@@ -0,0 +1,14 @@
+<!--公共组件之头部样式-->
+<template>
+  <h1>header</h1>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+
+</style>
